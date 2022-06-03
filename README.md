140a267e256899fdf9eee39f2fa80c2332de5821
luczoxz <Lucz0xxZ@gmail.com>
2022-06-03 19:14:25 +0200

    Added basic functionality

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# simple-cat-api
+# Images generator
 
-A simple cat api 4-fun project in typescript.
+A simple image api 4-fun project in typescript.
+
+## Installation
+To install, you have to compile the typescript files. You can do that by using:
+```sh
+tsc
+```
+Then, you can run the script anytime using:
+```sh
+node . --port <port>
+```
+You don't have to specify the port, it defaults to 3000.
+
+## Usage
+
+### Adding images
+To add images, put them in the `./images` directory. Each image has to be in the following formats: `jpg, jpeg, png`. That's it.
+
+### Possible requests
+
+#### `GET /api/random`
+Generates a random image. No parameters required. Returns an image ID.
+
+#### `GET /images?id=<id>`
+Returns the image that has the specified ID. The only required parameter is `id`. For example: `GET /images?id=1509`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("test")
+import express from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
+import minimist from 'minimist';
+
+const args = minimist(process.argv.slice(2))
+
+const app = express()
+let port = 3000
+if(args.port) port = args.port
+
+const extensions = ['.jpg', '.jpeg', '.png']
+const files = fs.readdirSync("./images/").filter(function(file) {
+    return extensions.includes(path.extname(file).toLowerCase());
+});
+const max = `${files.length}`
+
+
+app.get('/api/random', (req, res) => {
+  const num = Math.floor(Math.random() * (files.length - 0)) + 0;
+  res.json({message: "Random image ID generated", id: num})
+})
+
+app.get('/images', (req, res) => {
+  if(!req.query.id) return res.json({message: "Image ID not specified."})
+  if(req.query.id >= max || req.query.id < '0') return res.json({code: 404, 
+    message: "An image with the specified ID does not exist."
+  })
+  const id = parseInt(req.query.id.toString())
+  res.sendFile(`images/${files[id]}`, { root: "./" })
+  
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`)
+})
